ae886497060dfc0845919de37addb8a03390b00c
andiy <129535413+andriyshykoryak@users.noreply.github.com>
2023-08-11 17:58:11 +0300

    Add files via upload

--- Logika/m2/u1/CrazyPeople.дод/main.py
+++ Logika/m2/u1/CrazyPeople.дод/main.py
@@ -8,31 +8,34 @@ main_window = QWidget()
 
 text = QLabel('Натисни, щоб дізнатись переможця')
 winner = QLabel('?')
-
+winner1 = QLabel('?')
 button = QPushButton('Згенерувати')
 
 line = QVBoxLayout()
 line.addWidget(text,alignment=Qt.AlignCenter)
 line.addWidget(winner,alignment=Qt.AlignCenter)
+line.addWidget(winner1,alignment=Qt.AlignCenter)
 line.addWidget(button,alignment=Qt.AlignCenter)
 
 def win():
     ran = randint(1,100)
-    
-
+    ran1 = randint(1,100)
+    if ran == ran1:
+        asi = QLabel('Ти виграв')
+        line.addWidget(asi,alignment=Qt.AlignCenter)
+    else:
+        asi = QLabel('Ти Програв')
+        line.addWidget(asi,alignment=Qt.AlignCenter)
+        
+        
 
     winner.setText(str(ran))
-    
+    winner1.setText(str(ran1))
 
 button.clicked.connect(win)
 
 
 main_window.setLayout(line)
-
-
-
-
-
 
 main_window.show()
 
