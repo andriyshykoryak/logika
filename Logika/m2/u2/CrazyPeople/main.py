4b8f230beabd240768d4f8423036969bf13d534f
andiy <129535413+andriyshykoryak@users.noreply.github.com>
2023-08-11 19:15:57 +0300

    Add files via upload

--- Logika/m2/u2/CrazyPeople/main.py
+++ Logika/m2/u2/CrazyPeople/main.py
@@ -14,22 +14,34 @@ line = QVBoxLayout()
 line_gorisontal1 = QHBoxLayout()
 line_gorisontal2 = QHBoxLayout()
 line_gorisontal3 = QHBoxLayout()
-line_gorisontal1.addWidget(question)
+line_gorisontal1.addWidget(question,alignment=Qt.AlignCenter)
 
 
-line_gorisontal2.addWidget(ans1)
-line_gorisontal2.addWidget(ans2)
+line_gorisontal2.addWidget(ans1,alignment=Qt.AlignCenter)
+line_gorisontal2.addWidget(ans2,alignment=Qt.AlignCenter)
 
 
-line_gorisontal3.addWidget(ans3)
-line_gorisontal3.addWidget(ans4)
+line_gorisontal3.addWidget(ans3,alignment=Qt.AlignCenter)
+line_gorisontal3.addWidget(ans4,alignment=Qt.AlignCenter)
 
 
 
 line.addLayout(line_gorisontal1)
 line.addLayout(line_gorisontal2)
 line.addLayout(line_gorisontal3)
+def win():
+    victory_window = QMessageBox()
+    victory_window.setText('Вітаю\nТи виграв!')
+    victory_window.exec_()
+def lose():
+    lose_window = QMessageBox()
+    lose_window.setText('Нажаль\nТи програв!')
+    lose_window.exec_()
 
+ans1.clicked.connect(win)
+ans2.clicked.connect(lose)
+ans3.clicked.connect(lose)
+ans4.clicked.connect(lose)
 main_window.setLayout(line)
 
 main_window.show()
