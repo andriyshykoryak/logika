c9ddf9d7a6119d8e8947a1ce2f8ce6cb634e13a6
andiy <129535413+andriyshykoryak@users.noreply.github.com>
2023-09-18 19:20:48 +0300

    Add files via upload

---
+++ Logika/m3/u1/home_work.py
@@ -0,0 +1,24 @@
+import time
+start_time = time.time()
+class Student():
+   def __init__(self,surname,name,grade) -> None:
+      self.surname = surname
+      self.name = name
+      self.grade = grade
+
+students = []
+with open('students_large.txt', 'r', encoding='utf-8') as file:
+   for line in file:
+      data = line.split(' ')
+      obj = Student(data[0],data[1],int(data[2]))
+      students.append(obj)
+marks = []
+for s in students:
+#    if s.grade == 5:
+#       print(s.surname)
+   marks.append(s.grade)
+avereage_rate = sum(marks)/len(marks)
+print(avereage_rate)
+
+new_time = time.time() - start_time
+print('Робота програми:',new_time)
