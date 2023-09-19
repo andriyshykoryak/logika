7f631ac64b002b3c85ceec1758a038dd4ced0ae6
andiy <129535413+andriyshykoryak@users.noreply.github.com>
2023-09-19 16:53:47 +0300

    Add files via upload

--- Logika/m3/u1/home_work.py
+++ Logika/m3/u1/home_work.py
@@ -6,19 +6,20 @@ class Student():
       self.name = name
       self.grade = grade
 
+
+marks = []
 students = []
 with open('students_large.txt', 'r', encoding='utf-8') as file:
    for line in file:
       data = line.split(' ')
       obj = Student(data[0],data[1],int(data[2]))
       students.append(obj)
-marks = []
 for s in students:
-#    if s.grade == 5:
-#       print(s.surname)
+   if s.grade == 5:
+      print(s.surname)
    marks.append(s.grade)
 avereage_rate = sum(marks)/len(marks)
-print(avereage_rate)
+print('Середня оцінка',avereage_rate)
 
 new_time = time.time() - start_time
 print('Робота програми:',new_time)
--- Logika/m3/u1/new_main.py
+++ Logika/m3/u1/new_main.py
@@ -1,3 +1,6 @@
+import time
+start_time = time.time()
+
 class Student():
    def __init__(self,surname,name,grade) -> None:
       self.surname = surname
@@ -12,8 +15,10 @@ with open('new_file.txt', 'r', encoding='utf-8') as file:
       students.append(obj)
 marks = []
 for s in students:
-#    if s.grade == 5:
-#       print(s.surname)
+   if s.grade == 5:
+      print('Найкращі учні',s.surname)
    marks.append(s.grade)
 avereage_rate = sum(marks)/len(marks)
 print(avereage_rate)
+new_time = time.time() - start_time
+print('Робота програми:',new_time)
